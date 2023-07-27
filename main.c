55a0d553ed03477a07ab773b32236304b86b5d7e
A-asava <adikasaid996@gmail.com>
2023-07-27 12:17:24 +0000

    bug fixing

--- main.c
+++ main.c
@@ -1,64 +1,87 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
- * main - checks if the shell has been called
+ * main - Entry point for the simple shell program.
  *
- * Description: Initializes the command line interpreter which reads input, parses and executes it, 
- * 				then loops to read the next command. 
- * Return: 0 on success, appropriate error otherwise
+ * Description:
+ * This function implements a basic shell that repeatedly reads commands from
+ * the user or from input redirection, processes them, and executes the
+ * corresponding programs. It uses a prompt to indicate user input when reading
+ * from the terminal. The shell supports built-in commands "exit" and "env",
+ * as well as executing external commands in the system's PATH.
+ *
+ * Return:
+ * 0 on success, or an appropriate error code if an error occurs.
  */
 int main(void)
 {
-	char *prompt = "#cisfun$ ";
-	char *input = NULL;
-	size_t buffsize = 0;
-	char **args, *command;
-	int tokens, i;
+    char *shellPrompt = "#cisfun$ ";
+    char *inputLine = NULL;
+    size_t bufferSize = 0;
+    ssize_t numCharsRead;
+    char **args;
+    int numTokens;
+    char *command;
+    int i;
 
-	if (!isatty(STDIN_FILENO))
-		prompt = "";
-	
-	while (1)
-	{
-		buffsize = 0;
-		write(STDOUT_FILENO, prompt, strlen(prompt));
+    if (!isatty(STDIN_FILENO))
+    {
+        shellPrompt = "";
+    }
 
-		if ((getline(&input, &buffsize, stdin)) == -1)
-		{
-			write(STDOUT_FILENO, "Shell Exiting...\n", 17);
-			break;
-		}
-		
-		command = split_input(input);
-		while (command != NULL)
-		{
-			parseInput(command, &args, &tokens);
-			if (tokens > 0)
-			{
-				if (strcmp(args[0], "exit") == 0)
-				{
-					write(STDOUT_FILENO, "Exiting shell...\n", 17);
-					for (i = 0; i < tokens; i++)
-						free(args[i]);
-					free(args);
-					free(input);
-					return (0);
-				}
-				else if (strcmp(args[0], "env") == 0)
-				{
-					print_environment();
-				}
-				else
-				{
-					execmd(args);
-				}
-			}
-			for (i = 0; i < tokens; i++)
-				free(args[i]);
-			free(args);
-			command = split_input(NULL);
-		}
-		free(input);
-	}
-	return (0);
+    while (1)
+    {
+        bufferSize = 0;
+        write(STDOUT_FILENO, shellPrompt, strlen(shellPrompt));
+        numCharsRead = getline(&inputLine, &bufferSize, stdin);
+
+        if (numCharsRead == -1)
+        {
+            break;
+        }
+
+        command = split_input(inputLine);
+        while (command != NULL)
+        {
+            parseInput(command, &args, &numTokens);
+
+            if (numTokens > 0)
+            {
+                if (strcmp(args[0], "exit") == 0)
+                {
+                    for (i = 0; i < numTokens; i++)
+                    {
+                        free(args[i]);
+                    }
+                    free(args);
+                    free(inputLine);
+                    return (0);
+                }
+                else if (strcmp(args[0], "env") == 0)
+                {
+                    print_environment();
+                }
+                else
+                {
+                    execmd(args);
+                }
+            }
+            for (i = 0; i < numTokens; i++)
+            {
+                free(args[i]);
+            }
+            free(args);
+
+            command = split_input(NULL);
+        }
+
+        free(inputLine);
+    }
+
+    return (0);
 }
+
